d7a7d14a7b1f78ee8efccf4a95c2eb792741411c
Pablo Ramirez <pablillo.es@gmail.com>
2013-11-13 20:52:41 +0100

    filtrando por estado

--- src/client/www/js/init.js
+++ src/client/www/js/init.js
@@ -14,13 +14,23 @@ window.onload = function(){
          var state = TODO_APP.itemsLeft() > 0;
          checkAll(state);
      };
+     
+     window.onhashchange = function(){
+         //console.log(window.location.hash);
+         var pattern = "#/";
+        var pos = window.location.hash.indexOf(pattern);
+        var filter = window.location.hash.substring(pos + pattern.length);
+
+        TODO_APP.filterTodos(filter);
+        render();
+     };
 };
 
 function addTodo(text){
     var todo = TODO_APP.addTodo(text);
     
     var li = document.createElement('li');
-    li.id = 'todo_'+todo.getId;
+    li.id = 'todo_' + todo.getId();
        
         var check = document.createElement("input");
         check.type = 'checkbox';
@@ -80,17 +90,41 @@ function render(){
     var itemsLeft = document.getElementById('items-left');
     var clearCompleted = document.getElementById('clear-completed');
     
+    //MOSTRAR CONTADOR DE TAREAS PENDIENTES
     if(TODO_APP.countTodos()>0){
         itemsLeft.style.display = 'block';
         itemsLeft.innerHTML = TODO_APP.itemsLeft() + 'Item' +(TODO_APP.itemsLeft()!==1?'s':'')+' left';
     }else{
-        itemsLeft.sytle.display = 'none';
+       itemsLeft.style.display = 'none';
     }
     
+    //MOSTRAR BOTON DE BORRAR TAREAS COMPLETADAS
     if(TODO_APP.countTodos() - TODO_APP.itemsLeft()>0){
         clearCompleted.style.display='block';
         clearCompleted.innerHTML='Clear completed ('+(TODO_APP.countTodos()-TODO_APP.itemsLeft())+')';
     }else{
         clearCompleted.style.display='none';
+    }
+    
+    //FILTRAR POR ESTADO DE TAREAS
+    var todos = document.getElementById('todos');
+    var i = 0;
+    for (; i < todos.children.length; i++) {
+        var li = todos.children[i];
+        var regExp = /^todo_([0-9]+)$/;
+        var match = li.id.match(regExp);
+        if (match && match.length > 0) {
+            var id = match[1];
+            var todo = TODO_APP.getTodo(id);
+            if (todo) {
+                if (todo.isVisible()) {
+                    li.style.display = 'block';
+                } else {
+                    li.style.display = 'none';
+                }
+            }
+        }
+
+
     }
 }
